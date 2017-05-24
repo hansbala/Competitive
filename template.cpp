f73028a1f62da4e9135d9edff25dd676137044d3
Hans Bala <coolhansbala@gmail.com>
2017-05-24 14:11:12 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,12 +1,12 @@
 #include <bits/stdc++.h>
-
+#define rf freopen("inp.in", "r", stdin)
+typedef long long int ll;
 using namespace std;
 
 int main(){
+	//rf;
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	
 	return 0;
 }
-	
-	
